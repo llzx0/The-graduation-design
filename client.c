ba21845ebdadebbf5c049411950e009b8447af53
llzx0 <314040307@qq.com>
2015-05-26 01:05:47 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,49 +1,181 @@
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <unistd.h>
+
+#include <sys/socket.h>
+
+#include <sys/types.h>
+
 #include <netinet/in.h>
 
-#define BUFFSIZE 4096
+#include <arpa/inet.h>
 
-void DieWithError(char *errorMessage){
-perror(errorMessage);
-exit(1);
+
+
+#define SIZE 8192
+
+#define MSG_SIZE (SIZE-4)
+
+
+
+typedef struct tag_mag// 
+
+{
+
+    int msg_len ;
+
+    char msg_buf[MSG_SIZE];
+
+}MSG, *pMSG;
+
+
+
+int main(int argc, char* argv[])
+
+{
+
+    int sock;
+
+    struct sockaddr_in echoserver;
+
+    unsigned int echolen;
+
+    int received = 0;
+
+
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+
+    {
+
+        perror("Failed to create socket");
+
+        exit(1);
+
+    }
+
+    
+
+
+
+    memset(&echoserver, 0, sizeof(echoserver));      
+
+    echoserver.sin_family = AF_INET;               
+
+    echoserver.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+
+    echoserver.sin_port = htons(5656);
+
+    bind(sock, (struct sockaddr *) &echoserver, sizeof(echoserver));
+
+    if (connect(sock,(struct sockaddr *) &echoserver,sizeof(echoserver)) < 0) 
+
+    {
+
+        perror("Failed to connect with server");
+
+
+
+        exit(1);
+
+    }
+
+
+
+    MSG my_msg ;
+
+    memset(&my_msg, 0, sizeof(MSG));
+
+    strcpy(my_msg.msg_buf, "connected to the server");
+
+    my_msg.msg_len = strlen(my_msg.msg_buf);
+
+    st_send(NULL, sock, &my_msg, 4 + my_msg.msg_len );
+
+    memset(&my_msg, 0, sizeof(MSG));
+
+    st_recv(NULL, sock, &my_msg, 4);
+
+    st_recv(NULL, sock, &my_msg.msg_buf, my_msg.msg_len);
+
+    printf("recv from server : %s \n", my_msg.msg_buf);
+
+    
+
+ 
+
+    memset(&my_msg, 0, sizeof(MSG));
+
+    st_send(NULL, sock, &my_msg, 4 + my_msg.msg_len);
+
+    close(sock);
+
 }
 
-int main(int argc,char *argv[]){
-int sock;
-struct sockaddr_in echoserver;
-char buffer[BUFFSIZE];
-unsigned int echolen;
-int received=0;
-if(argc!=4){
-DieWithError("USAGE:simple_client <server_ip> <port> <request>\n");
-}
-if((sock=socket(PF_INET,SOCK_STREAM,IPPROTO_TCP))<0){
-DieWithError("failed to creat socket");
-}
-memset(&echoserver,0,sizeof(echoserver));
-echoserver.sin_family=AF_INET;
-echoserver.sin_addr.s_addr=inet_addr(argv[1]);
-echoserver.sin_port=htons(atoi(argv[2]));
+void st_recv( int *recv_len, int peer_sfd,  void *base, int len)
 
-if(connect(sock,(struct sockaddr *)&echoserver,sizeof(echoserver))<0){
-DieWithError("failed to connect with server");
+{
+
+    int recvn;
+
+    int recv_sum = 0;
+
+    while(recv_sum < len)
+
+    {
+
+        recvn = recv(peer_sfd, base + recv_sum, len - recv_sum, 0);
+
+        recv_sum += recvn;
+
+    }
+
+    if(recv_len != NULL)
+
+    {
+
+        *recv_len = recv_sum;
+
+    }
+
 }
-char *msg=argv[3];
-echolen=strlen(msg);
-if(send(sock,msg,echolen,0)!=echolen){
-DieWithError("send package error");
+
+
+
+void st_send( int *send_len, int peer_sfd, void *base, int len)
+
+{
+
+    int sendn;
+
+    int send_sum = 0;
+
+    while(send_sum < len)
+
+    {
+
+        sendn = send(peer_sfd, base + send_sum, len - send_sum, 0);
+
+        send_sum += sendn;
+
+    }
+
+    if(send_len != NULL)
+
+    {
+
+        *send_len = send_sum;
+
+    }
+
 }
-printf("received HTTP response from %s:\n",argv[2]);
-int bytes;
-while((bytes=recv(sock,buffer,BUFFSIZE-1,0))>0){
-buffer[bytes]='\0';
-printf("%s\n",buffer);
-}
-close(sock);
-exit(0);
-}
+
+   
+
+ 
+
+    
