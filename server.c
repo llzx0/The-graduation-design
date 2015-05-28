23c5ff2fe7d7c23b1e045ab56a99f0ebcb51b1f7
llzx0 <314040307@qq.com>
2015-05-28 08:05:25 +0800

    Update server.c

--- server.c
+++ server.c
@@ -1,17 +1,22 @@
 #include <curses.h>
 #include "main.h"
+
 #define ENTER 10
 #define ESCAPE 27
+#define QUIT_NOW 65535
+#define IDLE_NUM 10
 void init_curses() 
 { 
     initscr();
     start_color();
+
     init_pair(1,COLOR_WHITE,COLOR_BLUE); 
     init_pair(2,COLOR_BLUE,COLOR_WHITE);
     init_pair(3,COLOR_RED,COLOR_WHITE); 
     init_pair(4,COLOR_GREEN,COLOR_BLUE);
     curs_set(0); 
     noecho(); 
+    //echo(); 
     keypad(stdscr,TRUE); 
 } 
 void draw_menubar(WINDOW *menubar)
@@ -21,6 +26,7 @@ void draw_menubar(WINDOW *menubar)
     wattron(menubar,COLOR_PAIR(3));
     waddstr(menubar,"(F2)");
     wattroff(menubar,COLOR_PAIR(3));
+
 }
 WINDOW **draw_menu(int start_col)
 {
@@ -32,6 +38,8 @@ WINDOW **draw_menu(int start_col)
     box(items[0],ACS_VLINE,ACS_HLINE);
     items[1]=subwin(items[0],1,17,3,start_col+1);
     items[2]=subwin(items[0],1,17,4,start_col+1);
+
+
     wprintw(items[1],"SETTING");
     wprintw(items[2],"EXIT");
     wbkgd(items[1],COLOR_PAIR(2));
@@ -84,6 +92,7 @@ void updatedlog(char* str)
     if(log_index==1)
     {
         werase(alertWindow);
+        //move(5,2);
     }
     if(log_index>7)
     {
@@ -94,8 +103,14 @@ void updatedlog(char* str)
     curs_set(0);
     noecho();
     werase(querybox);
+    
+    //wprintw(alertWindow,"%s\n",str );
+
     box(alertWindow,0,0);
+
     wprintw(alertWindow," %s\n",str );
+    //mvwaddstr(alertWindow,2,log_index,str);
+    // mvwaddstr(alertWindow,4,1,"info");
     wrefresh(alertWindow);
     touchwin(stdscr);
     refresh();
@@ -105,8 +120,10 @@ int main()
 {
     int key,ch;
      int y,x;
+    
     WINDOW *wyes,*wno,*winput;
     init_curses();
+    //bkgd(COLOR_PAIR(1));
     menubar=subwin(stdscr,1,80,1,0);
     messagebar=subwin(stdscr,1,79,23,1);
     draw_menubar(menubar);
@@ -122,13 +139,17 @@ int main()
     alertWindow = subwin(stdscr,16,80,2,2); 
     wyes = newwin(8,10,y+2,x+1); 
     querybox = subwin(stdscr,8,40,y,x); 
+    
     curs_set(1);
     echo();
+
     box(querybox,0,0);
-    mvwaddstr(querybox,2,1,"please input a num.");
+    mvwaddstr(querybox,2,1,"please input peersize and poolsize)");
+   
     do {
         int selected_item;
         WINDOW **menu_items;
+
         key=getch();
         werase(messagebar);
         wrefresh(messagebar);
@@ -144,6 +165,8 @@ int main()
                     int nums;
                     int poolsize;
                     move(y+3,x+2);
+                 
+                 
                     touchwin(stdscr);
                     refresh();
                     scanw("%d",&nums);
@@ -151,52 +174,54 @@ int main()
                     scanw("%d",&poolsize);
                     refresh();
                     noecho();
-                    werase(messagebar);
-                    wrefresh(messagebar);
-                    touchwin(stdscr);
-                    refresh();
                     task(messagebar,nums,poolsize);
+
                 }
+
             }
+
+
         }
+
+
     } while (key!=ESCAPE);
+
     delwin(alertWindow);
     delwin(menubar);
     delwin(messagebar);
     endwin();
     return 0;
-}
-typedef struct {
-    pid_t pid;
-    char status; // 'n' means new request; 'f' means finish the request
-} REPORT;
-//paccnum 单进程能处理的数, 
-//poolsize 内存池大小 
-int task(WINDOW *messagebars,int paccnum,int poolsize)
 
+int task(WINDOW *messagebars,int paccnum,int poolsize)
 {
-    int quenesize = (paccnum-1)*poolsize;
+   
+    int queuesize = (paccnum-1)*poolsize;
     updatedlog("this the log window!");
-   //队列quenesize的大小 应该 为 poolsize *(paccunum-1)
-    pNODE cQueue = (pNODE)calloc(quenesize, sizeof(NODE)) ;// 一个队列（是用数组实现的），保存的是子进程的列表。
-    //上面只是定义了队列的最大大小 但是预先创建还是要创建 poolsize 个进程
+   
+    pNODE cQueue = (pNODE)calloc(queuesize, sizeof(NODE)) ;
     createchild(cQueue, poolsize);
+
     struct  sockaddr_in servaddr;
     int fd_listen, fd_client ;
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(5656);
+
     fd_listen = socket(AF_INET, SOCK_STREAM, 0);
     bind(fd_listen, (struct sockaddr *) &servaddr, sizeof(servaddr));
+ 
     listen(fd_listen, 1000);
+
+    
     fd_set readset, readyset ;
+  
     int index ;
     strcpy(buff,"Init the Queue \n");
     updatedlog(buff);
+   
     int select_ret ;
     struct timeval tm ;
-    //所有的子进程数目，接收成功的
     static int all_clild_num =0;
     while(1)
     {
@@ -210,23 +235,24 @@ int task(WINDOW *messagebars,int paccnum,int poolsize)
         tm.tv_sec = 0 ;
         tm.tv_usec = 1000 ;
         readyset = readset ;
+        
         select_ret = select(1024, &readyset, NULL, NULL, &tm);
         if(select_ret == 0)        
         {
             continue ;
-        }else if(select_ret == -1) /* 信号 */
+        }else if(select_ret == -1) 
         {
                 exit(1);
+
         }else 
         {
-            //检查是不是有数据到来。如果是，就接受这个连接
             if(FD_ISSET(fd_listen, &readyset))
             {
-                fd_client = accept(fd_listen, NULL, NULL) ;    
-                //如果有新子进程连接过来，就把计数先加1 
+                fd_client = accept(fd_listen, NULL, NULL) ; 
                 all_clild_num++;
                 sprintf(buff,"have accept child process %d\n", fd_client);
                 updatedlog(buff);
+              
                 for(index = 0 ; index < poolsize; index++)
                 {
                     if(cQueue[index].s_state == ST_IDLE)
@@ -234,74 +260,106 @@ int task(WINDOW *messagebars,int paccnum,int poolsize)
                         sprintf(buff,"find a idle child process %d\n", cQueue[index].s_sfd);
                         updatedlog(buff);
                         write(cQueue[index].s_sfd, &index, 4);
-                        send_fd(cQueue[index].s_sfd, fd_client); /* 向空闲的子进程分配任务，将服务器accept返回的socket描述符发送给子进程*/
+                        send_fd(cQueue[index].s_sfd, fd_client); 
                         cQueue[index].s_state = ST_BUSY ;
-                        //有空闲的时候，证明这个被处理了，把记数－1；
+                        
                         all_clild_num--;
+
                         break ;
                     }
                 }
-
                 if (all_clild_num>0)
                 {
-                    if (poolsize <= quenesize)
+                    if (poolsize <= queuesize)
                     {
-                        pid_t pid ;
-                        int fds[2] ;//fds[0] - c  fds[1] - p
+
+                        pid_t pid;
+                        int fds[2];
                         socketpair(AF_LOCAL, SOCK_STREAM, 0, fds);
                         sprintf(buff,"have created child process %d\n", pid);
                         pid = fork() ;
+                        
                         updatedlog(buff);
-                        if(pid == 0)// child
+                        if(pid == 0)
                         {
                             close(fds[1]);         
                             childloop(fds[0]) ;  
+
                         }else 
-                        {
+                        {                           
                             cQueue[poolsize].s_sfd = fds[1] ;
                             cQueue[poolsize].s_state = ST_IDLE ;
                             close(fds[0]);     
                             poolsize++;   
                         }
+
                     }
+                    
+
                 }
                 close(fd_client);
             }
             int index ;
+           
+            
+            
             for(index = 0; index < poolsize; index++)
             {
                 if(FD_ISSET(cQueue[index].s_sfd, &readyset))
                 {
                     int val ;
                     read(cQueue[index].s_sfd, &val, 4);
-                    cQueue[index].s_state = ST_IDLE ;//把该队列中的进程状态设置为空闲
+                    cQueue[index].s_state = ST_IDLE;
                 }
             }
-        }
+
+            int idle_count =0;
+            for(index = 0 ; index < poolsize; index++)
+            {
+                if(cQueue[index].s_state == ST_IDLE)
+                {
+                    if (idle_count>=IDLE_NUM)
+                    {
+                        int quit_signal = QUIT_NOW;
+			cQueue[index].s_state = ST_BUSY ;
+                        write(cQueue[index].s_sfd, &quit_signal, 4);
+                        idle_count--;
+                        continue;
+                    }
+                    idle_count++;
+                }
+            }
+        }      
     }   
 }
+
 void createchild(pNODE arr, int cnt)
 {
     int index ; 
     for(index = 0; index < cnt; index ++)
     {
-        pid_t pid ;
-        int fds[2] ;//fds[0] - c  fds[1] - p
+       
+        pid_t pid;
+        int fds[2];
         socketpair(AF_LOCAL, SOCK_STREAM, 0, fds);
         sprintf(buff,"have created child process %d\n", pid);
         pid = fork() ;
+        
         updatedlog(buff);
-        if(pid == 0)
+        if(pid == 0)// child
         {
             close(fds[1]);         
             childloop(fds[0]) ;  
+
         }else 
         {
             arr[index].s_sfd = fds[1] ;
             arr[index].s_state = ST_IDLE ;
             close(fds[0]);        
         }
+
     }
+
 }
 void childloop(int sfd)
 {
@@ -312,18 +370,19 @@ void childloop(int sfd)
     while(1)
     {
         readn = read(sfd, &flag, 4);
+        if (flag==QUIT_NOW)
+        {
+            exit(0);
+        }
         sprintf(buff,"have created child process %d\n", flag);
         updatedlog(buff);
         recv_fd(sfd, &fd_client);
         handle_request(fd_client);
+        close(fd_client);
         write(sfd, &pid, sizeof(pid));
     }
 }
-
-/* recv and send */
-
 void st_recv( int *recv_len, int peer_sfd,  void *base, int len)
-
 {
     int recvn;
     int recv_sum = 0;
@@ -337,6 +396,7 @@ void st_recv( int *recv_len, int peer_sfd,  void *base, int len)
         *recv_len = recv_sum;
     }
 }
+
 void st_send( int *send_len, int peer_sfd, void *base, int len)
 {
     int sendn;
@@ -353,6 +413,7 @@ void st_send( int *send_len, int peer_sfd, void *base, int len)
 }
 void handle_request(int sfd)
 {    
+
     MSG my_msg ;
     int recvn ;
     while(1)
@@ -363,52 +424,69 @@ void handle_request(int sfd)
         {
             break ;
         }
-    st_recv(NULL, sfd, my_msg.msg_buf, my_msg.msg_len);
-    st_send(NULL, sfd, &my_msg, my_msg.msg_len + 4);
+        st_recv(NULL, sfd, my_msg.msg_buf, my_msg.msg_len);
+        st_send(NULL, sfd, &my_msg, my_msg.msg_len + 4);
+
     }
+
 }
 void send_fd(int sfd, int fd_file) 
 {
     struct msghdr my_msg ;
     memset(&my_msg, 0, sizeof(my_msg));
+    
     struct iovec bufs[1] ;
     char buf[256] = "OK\n";
     bufs[0].iov_base = buf ;
-    bufs[0].iov_len = strlen(buf);
+    bufs[0].iov_len = strlen(buf) ;
+    
     my_msg.msg_name = NULL ;
     my_msg.msg_namelen = 0 ;
     my_msg.msg_iov = bufs ;
     my_msg.msg_iovlen = 1 ;
     my_msg.msg_flags = 0 ;
+
     struct cmsghdr *p  ;
-    int cmsg_len = CMSG_LEN(sizeof(int)) ;     /* 所传为文件描述符，因此sizeof(int) */
+    int cmsg_len = CMSG_LEN(sizeof(int)) ;
     p = (struct cmsghdr*)calloc(1, cmsg_len) ;
     p -> cmsg_len = cmsg_len ;
     p -> cmsg_level = SOL_SOCKET ;
     p -> cmsg_type = SCM_RIGHTS ;
     *(int*)CMSG_DATA(p) = fd_file ;
+    
     my_msg.msg_control = p ;
     my_msg.msg_controllen = cmsg_len ;
+    
     sendmsg(sfd, &my_msg, 0);
+    
 }
 void recv_fd(int sfd, int* fd_file) 
 {
     struct msghdr msg ;
+    
     struct iovec iov[1] ;
     char buf1[256]="" ;
     iov[0].iov_base = buf1 ;
     iov[0].iov_len = 31 ;
+
     msg.msg_name = NULL ;
     msg.msg_namelen = 0 ;
     msg.msg_iov = iov ;
     msg.msg_iovlen = 2 ;
     msg.msg_flags = 0 ;
+    
     struct cmsghdr *p  ;
     int cmsg_len = CMSG_LEN(sizeof(int)) ;
     p = (struct cmsghdr*)calloc(1, cmsg_len) ;
     msg.msg_control = p ;
     msg.msg_controllen = cmsg_len ;
+    
     int recvn ;
     recvn = recvmsg(sfd, &msg, 0);
+    
     *fd_file = *(int*)CMSG_DATA((struct cmsghdr*)msg.msg_control);  
+    
+    
+
 }
+ 
